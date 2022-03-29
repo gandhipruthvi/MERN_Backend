f4a9329a079a5d72969797590d623856e151c49d
Kuldip-1101 <n01415804@humber.ca>
2022-03-29 16:48:58 -0400

    FAQ Update

---
+++ models/UserFeedback.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const feedbackSchema = Schema({
+  name: {
+    type: String,
+    require: true,
+  },
+  lastname: {
+    type: String,
+    require: true,
+  },
+  email: {
+    type: String,
+    require: true,
+    unique: true,
+  },
+  feedback: {
+    type: String,
+  },
+});
+
+module.exports = mongoose.model("UserFeedback", feedbackSchema);
