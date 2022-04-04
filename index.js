cd9f418faa5391735161ed0be9cdb8e7870fc479
gandhipruthvi <gandhi.pruthvi@gmail.com>
2022-04-04 19:48:25 -0400

    small changes

--- index.js
+++ index.js
@@ -2,14 +2,14 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
 const cors = require("cors");
-const chat = require("./controllers/chat");
+const chat = require("./controller/chat");
 const connectDB = require("./config/connectDB");
 const userRoute = require("./routes/userRoutes");
 const productRoute = require("./routes/productRoutes");
 const authRoute = require("./routes/authRoutes");
 const feedbackRoute = require("./routes/feedbackRoutes");
-const faqRoute = require('./routes/faqRoutes');
-const { Server } = require('socket.io');
+const faqRoute = require("./routes/faqRoutes");
+const { Server } = require("socket.io");
 const app = express();
 const http = require("http").createServer(app);
 
@@ -21,13 +21,12 @@ app.use(express.json());
 app.use(cors());
 app.use(express.static("public"));
 
-
 //-------------Forward to different Routes-------------
 app.use("/api/user", userRoute);
 app.use("/api/product", productRoute);
 app.use("/api/auth", authRoute);
 app.use("/api/userFeedback", feedbackRoute);
-app.use('/api/faqs', faqRoute);
+app.use("/api/faqs", faqRoute);
 
 //------------Listen server on port from environmental variable-----------------
 const PORT = process.env.PORT | 5000;
