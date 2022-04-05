6e4cf5fe68b98b23211a452f8ac83cd461039750
gandhipruthvi <gandhi.pruthvi@gmail.com>
2022-04-04 20:09:34 -0400

    small changes

--- index.js
+++ index.js
@@ -1,23 +1,17 @@
-
 //------------------Import------------------
 
 const express = require("express");
 const dotenv = require("dotenv").config();
 const cors = require("cors");
-const chat = require("./controller/chat");
+const chat = require("./controllers/chat");
 const connectDB = require("./config/connectDB");
 const userRoute = require("./routes/userRoutes");
 const productRoute = require("./routes/productRoutes");
 const authRoute = require("./routes/authRoutes");
 const feedbackRoute = require("./routes/feedbackRoutes");
-<<<<<<< HEAD
 const faqRoute = require("./routes/faqRoutes");
+const eventRoute = require("./routes/eventRoutes");
 const { Server } = require("socket.io");
-=======
-const faqRoute = require('./routes/faqRoutes');
-const eventRoute = require('./routes/eventRoutes');
-const { Server } = require('socket.io');
->>>>>>> ae28f3681f5cde73b4e7149f7efd4783cef2697d
 const app = express();
 const http = require("http").createServer(app);
 
@@ -27,9 +21,6 @@ app.use(express.static("public"));
 connectDB();
 
 app.use(express.json());
-
-
-
 
 //--------------Connect TO Database-----------------
 connectDB();
@@ -45,12 +36,8 @@ app.use("/api/user", userRoute);
 app.use("/api/product", productRoute);
 app.use("/api/auth", authRoute);
 app.use("/api/userFeedback", feedbackRoute);
-<<<<<<< HEAD
 app.use("/api/faqs", faqRoute);
-=======
-app.use('/api/faqs', faqRoute);
-app.use('/api/Events', eventRoute);
->>>>>>> ae28f3681f5cde73b4e7149f7efd4783cef2697d
+app.use("/api/Events", eventRoute);
 
 //------------Listen server on port from environmental variable-----------------
 const PORT = process.env.PORT | 5000;
